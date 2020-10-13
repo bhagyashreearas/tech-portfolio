86f798db22599bfac40755e0352678e7eddb964a
Bhagyashree Aras <63433929+bhagyashreearas@users.noreply.github.com>
2020-10-12 22:25:26 -0700

    Updated Tagline
    Updated Tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Bhagyashree Aras', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Enthusiastic and passionate technologist.',
     profilePhoto: 'img/profile.png',
     url: 'https://bhagyashreearas.github.io/', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
